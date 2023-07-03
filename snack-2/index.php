a81c10227660fcb81b78ec5d684831d563ed0935
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-07-03 12:59:23 +0200

    completed snack 2

---
+++ snack-2/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="get">
+        <input type="text" id="name" name="name">
+        <input type="text" id="mail" name="mail" >
+        <input type="number" id="age" name="age">
+    </form>
+</body>
+</html>
---
+++ snack-2/server.php
@@ -0,0 +1,24 @@
+<?php 
+   $name= $_GET['name'];
+   $mail=$_GET['mail'];
+   $age=$_GET['age'];
+   $needle=[".","@"]
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p><?php if (strlen($name) > 3 && in_array($needle,$mail) && is_numeric($age) ){
+        echo "accesso riuscito"
+        }
+        else{
+            echo "accesso negatp"
+        }
+    ?></p>
+</body>
+</html>
