e85df2a9c546cd272ab83624a8d5b32a8f918895
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-07-14 12:51:56 +0200

    added Hat class

---
+++ snack-5/hat.php
@@ -0,0 +1,13 @@
+<?php
+ 
+  class Hat extends Cloth{
+     public $color;
+
+
+     function__construct(String $material,String $color){
+       parent::function__construct($material);
+       $this-> color = $color;
+     }
+  }
+
+?>
