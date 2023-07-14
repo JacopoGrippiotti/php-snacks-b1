317f1a6a7242e9cdf050ab8b0f2d813b554d18a9
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-07-14 12:48:22 +0200

    added Shoe as a child of Indument in Shoe.php file

---
+++ snack-5/Shoe.php
@@ -0,0 +1,13 @@
+<?php
+   include_once __DIR__ . '/index.php'
+   class Shoe extends Cloth{
+   public $numberOfShoe;
+   
+   
+   function__construct(INT $numberOfShoe, String $material){
+    
+    parent::function__construct($material);
+    $this-> numberOfShoe = $numberOfShoe;
+   }
+  }
+?>
